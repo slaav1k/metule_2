68ad215bcc32767f6d1fee6c23734a69995f4a40
slaav1k <74726728+slaav1k@users.noreply.github.com>
2021-03-18 19:26:48 +0300

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -10,6 +10,11 @@ def index():
     return "HELLO WORLD! КРЫМ НАШ! ПУТИН СИЛА! ПАША ВЫЗДОРАВЛИВАЙ!"
 
 
+@app.route("love")
+def love():
+    return "I LOVE YOU MY BABY!"
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
